a8a9c0ab82dd07620862fd4ca22a18488e0c6e2a
OmidTavana <OmidtavanaCoder@gmail.com>
2022-04-10 09:56:48 +0430

    Init

---
+++ DontShowHiddens/DontShowHiddens/Main.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DontShowHiddens
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 45);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+    }
+}
---
+++ DontShowHiddens/DontShowHiddens/Main.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DontShowHiddens
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Task.Factory.StartNew(Do);
+        }
+        /// <summary>
+        /// Uncheck Hidden CheckBox in windows every 3 
+        /// secounds if given password is incorrect
+        /// </summary>
+        void Do()
+        {
+            //runs for ever
+            while (true)
+            {
+                //wait 3 sec
+                Task.Delay(3000);
+                if (Str != "omidtavana.com")
+                {
+                    Hidden("0");
+                }
+            }
+        }
+        public string Str { get { return textBox1.Text; } set { value = textBox1.Text; } }
+        /// <summary>
+        /// Change windows Hiddent Registry to hide or show hidden files
+        /// </summary>
+        /// <param name="value">0/1</param>
+        void Hidden(string value)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced", true);
+            key.SetValue("Hidden", value, RegistryValueKind.DWord);
+            key.Close();
+        }
+
+        private void WhileForm_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+    }
+}
